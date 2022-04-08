4b23995554f9da68637672c5f31d501bc134ec35
lynne921 <dkfls921@gmail.com>
2022-04-09 08:33:32 +0900

    HUCO-12 | [refactor] add annotation and edit feature

--- HandsUp/Assets/OpeningScene/Scripts/OpeneingManager.cs
+++ HandsUp/Assets/OpeningScene/Scripts/OpeneingManager.cs
@@ -3,15 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Text.RegularExpressions;
 
 public class OpeneingManager : MonoBehaviour
 {
     public GameObject[] Pages = new GameObject[4];
 
     private UserData userData;
+    //private PlayerData playerData;
 
     public InputField[] userDatas; // for sign in
     public InputField[] userDataField; // for sign up
+    public InputField[] userEditDataField; // for edit
 
     public void OnClickSignUpPageBtn()
     {
@@ -25,15 +28,23 @@ public class OpeneingManager : MonoBehaviour
 
     public void OnClickWithoutSignPageBtn()
     {
-        SetPageActive(2);
+        // 비회원 이용 버튼 클릭 시 닉네임 버튼의 text 값을 로그인으로 변경
         GameObject.Find("NickNameBtn").GetComponentInChildren<Text>().text = "로그인";
+        SetPageActive(2);
     }
 
     public void OnClickNicknamePageBtn()
     {
+        // 닉네임이 있으면(=회원이면) 편집 페이지로 이동, 없으면(=비회원이면) 로그인 페이지로 이동
+        //if (playerData.GetUserName() != null) 
+        //{
+        //	SetPageActive(3);
+        //}
+        //else
         SetPageActive(3);
     }
 
+    // 페이지 초기화
     private void SetPageActive(int index) // 0 : DefaultPage, 1 : SignUpPage 2 : MainPage 3 : EditPage
     {
         for (int i = 0; i < Pages.Length; i++)
@@ -43,9 +54,15 @@ public class OpeneingManager : MonoBehaviour
         Pages[index].SetActive(true);
     }
 
-    //TO-DO : user email form checking
     public void OnClickSignUpBtn()
     {
+        //user email form checking
+        string userEmail = userDataField[0].text;
+        bool valid = Regex.IsMatch(userEmail, @"[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?");
+        if (valid)
+        {
+            Debug.Log("Verified Email");
+        }
 
         //TO-DO : add PW Diffrent alarm
         if (!userDataField[3].text.Equals(userDataField[2].text))
@@ -60,6 +77,12 @@ public class OpeneingManager : MonoBehaviour
     //TO-DO : user signup api update, add alarms
     private void SignUp()
     {
+        //만약 playerData가 들어간다면 이렇게? 다만 password 부분은 아직 함수가 없어 주석처리 해놓음
+        //playerData = new PlayerData();
+        //playerData.SetUserEmail(userDataField[0].text);
+        //playerData.SetUserName(userDataField[1].text)
+        //playerData.SetUserPassword(userDataField[2].text);
+
         userData = new UserData();
         userData.email = userDataField[0].text;
         userData.userName = userDataField[1].text; 
@@ -114,16 +137,32 @@ public class OpeneingManager : MonoBehaviour
             else
             {
                 Debug.Log("Sucessful Sign In!");
+<<<<<<< Updated upstream
                 userData.userName = raw;
                 GameObject.Find("NickNameBtn").GetComponentInChildren<Text>().text = userData.userName;
                 SetPageActive(2);
+=======
+                SetPageActive(2);
+                // 닉네임 부분 띄워주기 userName은 따로 받지 않았기 때문에 1. playerData를 통해 받거나, 2. DB에서 받는다? -> playerData 활용
+                //GameObject.Find("NickNameBtn").GetComponentInChildren<Text>().text = playerData.GetUserName();
+>>>>>>> Stashed changes
             }
 
         }));
     }
+<<<<<<< Updated upstream
  
+=======
+
+    public void OnClickEditBtn()
+    {
+        EditInfo();
+    }
+
+>>>>>>> Stashed changes
     private void EditInfo()
     {
+        //정보를 받아서 바로 쏴주기 때문에 SignUp과 똑같지만 DB 질의문이 달라지는 구조라고 생각했습니다
         userData = new UserData();
         userData.userName = userDataField[0].text;
         userData.email = userDataField[1].text;
@@ -132,6 +171,7 @@ public class OpeneingManager : MonoBehaviour
         var req = JsonConvert.SerializeObject(userData);
         Debug.Log(req);
 
+        // Edit페이지 내 아이디, 닉네임 입력 창에 아이디와 닉네임 희미하게 띄워주기 위해
         GameObject.Find("IDField").GetComponentInChildren<Text>().text = userData.email;
         GameObject.Find("NickNameField").GetComponentInChildren<Text>().text = userData.userName;
 
