13ea8cf95adf13ebff6fcaacf31cc0315e8d1b75
sleesm <sleepingsm813@gmail.com>
2022-05-04 14:31:15 +0900

    HUCO-86 | [fix] fix the error of the dontdestory function - DontDestoryed object but different name

--- HandsUp/Assets/Shared/Scripts/DontDestroyed.cs
+++ HandsUp/Assets/Shared/Scripts/DontDestroyed.cs
@@ -6,15 +6,32 @@ public class DontDestroyed : MonoBehaviour
 {
     private void Awake()
     {
-        var obj = FindObjectsOfType<DontDestroyed>();
-
-        if (obj.Length == 1)
+        var objs = FindObjectsOfType<DontDestroyed>();
+        int count = 0;
+        foreach(var obj in objs)
         {
-            DontDestroyOnLoad(gameObject);
+            if (obj.name.Equals(gameObject.name))
+            {
+                count++;
+                if(count > 1)
+                {
+                    Destroy(gameObject);
+                    Debug.Log(obj.name);
+                }
+                else
+                {
+                    DontDestroyOnLoad(gameObject);
+                }
+            }
+        }
+/*        if (obj.Length == 1)
+        {
+                DontDestroyOnLoad(gameObject);
         }
         else
         {
-            Destroy(gameObject);
-        }
+            if(obj[0].name.Equals(gameObject.name))
+                Destroy(gameObject);
+        }*/
     }
 }
