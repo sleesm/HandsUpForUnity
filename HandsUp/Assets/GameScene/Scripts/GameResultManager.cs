c550a4bba0ee60f4444ab2b9b2d7ccb5e6aec168
sleesm <sleepingsm813@gmail.com>
2022-05-05 22:52:46 +0900

    HUCO-97 | [chore] change reulst to result

--- HandsUp/Assets/GameScene/Scripts/GameResultManager.cs
+++ HandsUp/Assets/GameScene/Scripts/GameResultManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class GameReulstManager : MonoBehaviour
+public class GameResultManager : MonoBehaviour
 {
 
     private CardManager cardManager;
