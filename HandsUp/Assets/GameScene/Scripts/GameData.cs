647d7afef027ce005a1b4e5a261a977fa7f14b30
icechocola05 <icechocola05@gmail.com>
2022-05-10 18:27:20 +0900

    HUCO-88 | [fix] make game data to scriptable object

--- HandsUp/Assets/GameScene/Scripts/GameData.cs
+++ HandsUp/Assets/GameScene/Scripts/GameData.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "GameData", menuName = "ScriptableObjects/gameData", order = 1)]
 public class GameData : ScriptableObject
 {
     public int gameVersion;
