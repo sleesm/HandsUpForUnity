233c4ee66676305151793930b89271004e348400
sleesm <sleepingsm813@gmail.com>
2022-05-15 12:26:44 +0900

    HUCO-104 | [feat] create function of getting Image byte array for sending it to server

--- HandsUp/Assets/CardViewScene/Scripts/ImageManager.cs
+++ HandsUp/Assets/CardViewScene/Scripts/ImageManager.cs
@@ -7,6 +7,8 @@ using UnityEngine.UI;
 public class ImageManager : MonoBehaviour
 {
     public RawImage cardImg;
+    private string currentImgByte;
+
     public void OnClickImgLoad()
     {
         NativeGallery.GetImageFromGallery((file) =>
@@ -34,5 +36,14 @@ public class ImageManager : MonoBehaviour
         Texture2D texture = new Texture2D(0, 0);
         texture.LoadImage(fileData);
         cardImg.texture = texture;
+
+        string jpgBase64 = System.Convert.ToBase64String(fileData);
+        currentImgByte = jpgBase64;
+
+    }
+
+    public string GetCurrentImgByte()
+    {
+        return currentImgByte;
     }
 }
