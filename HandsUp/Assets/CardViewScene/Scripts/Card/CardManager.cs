df96779bd18b184b3b6f42893ba1875eb594eda7
sleesm <sleepingsm813@gmail.com>
2022-05-16 21:47:51 +0900

    HUCO-104 | [chore] delete comment

--- HandsUp/Assets/CardViewScene/Scripts/Card/CardManager.cs
+++ HandsUp/Assets/CardViewScene/Scripts/Card/CardManager.cs
@@ -14,13 +14,11 @@ public class CardManager : MonoBehaviour
     public GameObject cardItem;
 
     private List<Card> cards;
-    //private List<Card> customCards;
 
     private void Awake()
     {
         playerManager = GameObject.Find("PlayerManager").GetComponent<PlayerManager>();
         cards = new List<Card>();
-        //customCards = new List<Card>();
     }
 
 
@@ -33,7 +31,6 @@ public class CardManager : MonoBehaviour
             if (!isGame)
                 DestoryCards();
             cards.Clear();
-            //customCards.Clear();
         }
 
         GetCardsFromServer(categoryId, isGame, path);
@@ -126,88 +123,6 @@ public class CardManager : MonoBehaviour
         return null;
     }
 
-
-
-/*    public void GetCustomCardsFromServer(int categoryId, int userId, bool isGame = false, string path = "")
-    {
-        CardData cardData = new CardData();
-        cardData.category_id = categoryId;
-        cardData.user_id = userId;
-
-        var req = JsonConvert.SerializeObject(cardData);
-
-        StartCoroutine(DataManager.sendDataToServer("category/card/custom", req, (raw) =>
-        {
-            Debug.Log(raw);
-            JObject applyJObj = JObject.Parse(raw);
-            foreach (JObject tmpCard in applyJObj["cards"])
-            {
-                Card tmp = new Card();
-                tmp.SetCardId((int)tmpCard["card_id"]);
-                tmp.SetName(tmpCard["card_name"].ToString());
-                tmp.SetCategoryId(categoryId);
-                tmp.SetImagePath(tmpCard["card_img_path"].ToString());
-
-                tmp.SetUserId(userId);
-
-                customCards.Add(tmp);
-            }
-
-            if (!isGame)
-                CreateNewCustomCardItems(customCards, path);
-            else
-                GameManager.isCustomCardLoaded = true;
-
-        }));
-    }
-
-    private void CreateNewCustomCardItems(List<Card> customCards, string path)
-    {
-        for (int i = 0; i < customCards.Count; i++)
-        {
-            GameObject newCardItem = Instantiate(cardItem, new Vector3(0, 0, 0), Quaternion.identity);
-            newCardItem.transform.SetParent(GameObject.Find("Canvas").transform.Find(path).transform.Find("CardsScrollView/Viewport/Content").transform);
-            newCardItem.transform.localScale = new Vector3(1, 1, 1);
-            newCardItem.GetComponent<Card>().SetImagePath(customCards[i].GetImagePath());
-            newCardItem.GetComponent<Card>().SetName(customCards[i].GetName());
-
-            newCardItem.GetComponentInChildren<Text>().text = customCards[i].GetName();
-            StartCoroutine(getImagesFromURL(customCards[i].GetImagePath(), newCardItem));
-        }
-    }
-
-    public List<Card> GetCustomCards()
-    {
-        return customCards;
-    }
-
-    public Card GetCustomCardInfo(int id)
-    {
-        foreach (Card tmp in customCards)
-        {
-            if (tmp.GetCardId().Equals(id))
-                return tmp;
-        }
-
-        return null;
-    }
-
-
-    public List<Card> GetAllCards()
-    {
-        List<Card> cardsTmp = new List<Card>();
-        foreach (Card tmp in cards)
-        {
-            cardsTmp.Add(tmp);
-        }
-        if(customCards.Count > 0)
-            foreach (Card tmp in customCards)
-            {
-                cardsTmp.Add(tmp);
-            }
-
-        return cardsTmp;
-    }*/
 
     public IEnumerator getImagesFromURL(string imgurl, GameObject item, bool isGame = false)
     {
